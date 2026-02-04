1c22c78cdd3f727dbead668fc3f4b34a76c3d4f1
Oleg <OI.Gomov@yandex.ru>
2026-02-04 12:12:27 +0300

    the LionParamTest is ready

---
+++ src/test/java/LionParamTest.java
@@ -0,0 +1,36 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class LionParamTest {
+
+    private String sex;
+    private boolean hasMane;
+
+    public LionParamTest(String sex, boolean hasMane) {
+        this.sex = sex;
+        this.hasMane = hasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getSex() {
+        return new Object[][] {
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
+
+    @Test
+    public void doesHaveManeTest() throws Exception {
+        Lion lion = new Lion(sex, new Feline());
+
+        boolean expected = hasMane;
+        boolean actual = lion.doesHaveMane();
+        assertEquals("Ошибка наличия гривы", expected, actual);
+    }
+}
