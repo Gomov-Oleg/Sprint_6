3768cf7e5534a8d5faeb0251f5ab47add61c6c52
Oleg <OI.Gomov@yandex.ru>
2026-02-04 09:55:34 +0300

    the CatTest is ready

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,36 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getSoundTest() {
+        Cat cat = new Cat(feline);
+
+        String actual = cat.getSound();
+        assertEquals("Звук животного не соответствует ожидаемому","Мяу", actual);
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Cat cat = new Cat(feline);
+
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> expectedResult = List.of("Животные", "Птицы", "Рыба");
+        List<String> actualResult = cat.getFood();
+        assertEquals("Некорректный результат", expectedResult, actualResult);
+    }
+}
